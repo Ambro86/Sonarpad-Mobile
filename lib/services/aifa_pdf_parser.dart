0a3c60b29faa1e0495c9ebc7e50a6eff81716622
Ambro86 <58132298+Ambro86@users.noreply.github.com>
2026-06-07 10:00:46 +0200

    Modified aifa parser for small fixes.

--- lib/services/aifa_pdf_parser.dart
+++ lib/services/aifa_pdf_parser.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'document_text_extractor.dart';
@@ -427,7 +426,6 @@ class AifaPdfParser {
     }
   }
 
-  @visibleForTesting
   static String extractSectionTextForTest(
     String text,
     AifaSectionType type,
